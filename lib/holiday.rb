22ca8e888bd368770ab96296c56b28e21b21f465
Sav Younts <savyounts@gmail.com>
2018-08-07 02:55:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_supplies.collect do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
-    holidays.to_s.split(_).capitalize
+    holidays.to_s.split.capitalize
     holidays.collect do |holiday, supplies|
       puts "  #{holiday.to_s.capitalize!}: #{supplies.join(", ")}"
     end
